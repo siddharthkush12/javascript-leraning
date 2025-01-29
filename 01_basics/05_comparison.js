0e2a11480adcd39eda8eed5607afabc9de82998b
siddharthkush12 <siddharth5044549@gmail.com>
2025-01-29 13:56:38 +0530

    comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,20 @@
+console.log("2"<1)    // it give true but always compare same datatype, it give unpredictable result
+console.log()
+
+
+//  **************************** avoid these comparison
+console.log(null>0)
+console.log(null>=0)
+console.log(null==0)
+console.log()
+
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined>0)
+console.log(undefined<0)
+console.log()
+//  *****************************
+
+
+console.log("2"===2)      //check datatype also
+console.log("2"!==2)
